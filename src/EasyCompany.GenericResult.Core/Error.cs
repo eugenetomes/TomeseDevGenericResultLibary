c76459a9483315c4c6b3298f540cecac0496d970
Eugene Tomes <eugenetomesdev@gmail.com>
2023-04-27 18:34:55 +0200

    Move Project

--- src/EasyCompany.GenericResult.Core/Error.cs
+++ src/EasyCompany.GenericResult.Core/Error.cs
@@ -1,4 +1,10 @@
-﻿namespace EasyCompany.GenericResult.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCompany.GenericResult.Core;
 
 public class Error : IEquatable<Error>
 {
